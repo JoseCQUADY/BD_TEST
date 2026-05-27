dcc0c6e52e30bdaabde13c0277e8f35209c6b61e
José Chi Quintal <josechiquintal12@gmail.com>
2026-05-27 12:11:38 -0600

    feature: improve logs

--- src/logger_manager.py
+++ src/logger_manager.py
@@ -3,14 +3,14 @@ from datetime import datetime
 from config.settings import LOGS_DIR
 
 def get_logger(module_name):
-    logger = logging.getLogger(f"TelephonyApp.{module_name}")
+    logger = logging.getLogger(f"{module_name}")
     
     if not logger.handlers:
         logger.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        log_file = LOGS_DIR / f"execution_{timestamp}.log"
+        log_file = LOGS_DIR / f"log_{timestamp}.log"
 
         file_handler = logging.FileHandler(log_file, encoding='utf-8')
         file_handler.setFormatter(formatter)
